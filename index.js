01eb30ae41b0ee5376324ea49e6a066c9bf4c45c
Isiaka Abdulahi <isiakaabd@gmail.com>
2021-03-21 15:05:08 +0100

    adding more JS features

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const cellElements = document.querySelectorAll(".cell");
 const board = document.getElementById("board");
-var selector= false;
+var selector;
 const X_CELL= "x";
-const Circle = "circle"
+const Circle = "circle";
 
 
+var possibleWins = [
+    [[0], [1], [2]], //horizontal 1
+    [[3], [4], [5]], //horizontal 2
+    [[6], [7], [8]], //horizontal   [cells[0], [3], cells[6]], //vertical 1
+    [[1], [4], [7]], //vertical 2
+    [[2], [5], [8]], //vertical   [cells[0], [4], cells[8]], //diagonal 1
+    [[6], [4], [2]]  //diagonal 2
+]
 
-cellElements.forEach(cell =>{
-    cell.addEventListener("click", handleClick, {once: true})
-})
+
+stateGame()
+function stateGame(){
+  selector = false;
+    cellElements.forEach(cell =>{
+        cell.addEventListener("click", handleClick, {once: true})
+    })
+    // to hover at the load of the game
+    boardHover()
+}
 
 
 function handleClick(e) {
     cell = e.target
     const selectorValue = selector? X_CELL: Circle
-     placeMark(cell, selectorValue)
-    // cell.classList.add(selectorValue)
+   cell.classList.add(selectorValue)
     switchValue()
     boardHover()
 }
-  function placeMark(cell,selectorValue){
-cell.classList.add(selectorValue)
-  }
+ 
 
 // for switching elector
 function switchValue (){
